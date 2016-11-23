15a1a57cdbcaf0a50662cd00190e112f310d1aad
Megt <megyltv@hotmail.com>
2016-11-23 14:35:37 -0500

    version 1.0.1

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -1,5 +1,12 @@
-var numero1 = 1;
-var numero2 = 2;
+//tecnologiaswebtmegan.imprimirSuma1y2()
 
-// Se corre con el comando node app.js (nombre del archivo es app.js)
-console.log(numero1 + numero2);
+module.exports = {
+    imprimirSuma1y2:imprimirSuma1y2,
+}
+
+function imprimirSuma1y2() {
+    var numero1 = 1;
+    var numero2 = 2;
+    // Se corre con el comando node app.js (nombre del archivo es app.js)
+    console.log(numero1 + numero2);
+}
---
+++ Nodejs2/node_modules/tecnologiaswebtmegan/app.js
@@ -0,0 +1,5 @@
+var numero1 = 1;
+var numero2 = 2;
+
+// Se corre con el comando node app.js (nombre del archivo es app.js)
+console.log(numero1 + numero2);
