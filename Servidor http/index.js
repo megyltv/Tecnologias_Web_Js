8950824273f64abe02895a2f848a480a95fd2fed
Megt <megyltv@hotmail.com>
2016-11-30 13:49:55 -0500

    get y post

--- Servidor http/index.js
+++ Servidor http/index.js
@@ -1,10 +1,21 @@
 var express = require('express')
 var app = express()
+var puerto=5050
 
 app.get('/TecnologiasWeb', function (req, res) {
-  res.send('Hello World!')
+  res.send('con javascript')
 })
 
-app.listen(5050, function () {
-  console.log('con javascript')
+app.listen(puerto, function () {
+  console.log('Example app listening on port '+puerto+'!')
+})
+
+var puerto=6000
+
+app.post('/TecnologiasWeb', function (req, res) {
+  res.send('con POST')
+})
+
+app.listen(puerto, function () {
+  console.log('Example app listening on port '+puerto+'!')
 })
