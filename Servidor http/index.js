6218cdf69f7444ce369bc94c6f3b4ec33b586e85
Megt <megyltv@hotmail.com>
2016-11-30 14:16:11 -0500

    aumento de parametro en cabecera

--- Servidor http/index.js
+++ Servidor http/index.js
@@ -1,21 +1,46 @@
 var express = require('express')
 var app = express()
-var puerto=5050
+var puerto = 5050
 
 app.get('/TecnologiasWeb', function (req, res) {
-  res.send('con javascript')
+    res.send('con javascript')
 })
 
 app.listen(puerto, function () {
-  console.log('Example app listening on port '+puerto+'!')
+    console.log('Example app listening on port ' + puerto + '!')
 })
 
-var puerto=6000
+puerto=6000;
 
 app.post('/TecnologiasWeb', function (req, res) {
-  res.send('con POST')
+    var usuario={
+        nombre: 'Megan',
+        cedula:'0000000000'
+    }
+    
+    usuario.apellido='Toinga';
+    usuario.mascotas=['Max', 'Misty', 'Dash'];
+    
+    console.log("HEADERS");
+    console.log("Lo que tengo en el request es: ");
+    console.log(req.headers);
+    console.log("Lo que tengo en el request es: ");
+    
+    res.append('Token','1234');
+    res.append('Usuario',usuario.nombre);
+    
+    
+    
+    
+    res.json(usuario);
+    
+    console.log(res.headers);
+    //res.send('con POST')
+    
+    
+
 })
 
 app.listen(puerto, function () {
-  console.log('Example app listening on port '+puerto+'!')
+    console.log('Example app listening on port ' + puerto + '!')
 })
