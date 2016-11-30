3d9fb417d122a670cb4f2cf4b9a192789ac4e0b0
Megt <megyltv@hotmail.com>
2016-11-30 14:42:06 -0500

    metodo con parametros

--- Servidor http/index.js
+++ Servidor http/index.js
@@ -1,43 +1,68 @@
-var express = require('express')
-var app = express()
-var puerto = 5050
+var express = require('express');
+var app = express();
+var puerto = 5050;
 
-app.get('/TecnologiasWeb', function (req, res) {
-    res.send('con javascript')
+var usuarios = [
+    {
+        id: 1,
+        nombre: 'Pepe',
+        cedula: 123456789
+    }, {
+        id: 2,
+        nombre: 'Carlos',
+        cedula: 789456123
+    }, {
+        id: 3,
+        nombre: 'Juan',
+        cedula: 456789123
+    }
+];
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.get('/Usuario', function (req, res) {
+
+    res.json(usuarios);
+    
+})
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    var idActual = req.params.idUsuario; //guardar parametros 
+    //Buscando en todo el arreglo
+    for (var i = 0; i < usuarios.length; i++) {
+        if (idActual == usuarios[i].id) {
+            //responder al usuario
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe usuario');
 })
 
 app.listen(puerto, function () {
     console.log('Example app listening on port ' + puerto + '!')
 })
 
-puerto=6000;
+puerto = 6000;
 
 app.post('/TecnologiasWeb', function (req, res) {
-    var usuario={
+
+    var parametros = req.params;
+    console.log(parametros);
+
+    var usuario = {
         nombre: 'Megan',
-        cedula:'0000000000'
+        cedula: '0000000000'
     }
-    
-    usuario.apellido='Toinga';
-    usuario.mascotas=['Max', 'Misty', 'Dash'];
-    
-    console.log("HEADERS");
-    console.log("Lo que tengo en el request es: ");
-    console.log(req.headers);
-    console.log("Lo que tengo en el request es: ");
-    
-    res.append('Token','1234');
-    res.append('Usuario',usuario.nombre);
-    
-    
-    
-    
+
+    res.append('Token', '1234');
+    res.append('Usuario', usuario.nombre);
+
     res.json(usuario);
-    
+
     console.log(res.headers);
     //res.send('con POST')
-    
-    
 
 })
 
