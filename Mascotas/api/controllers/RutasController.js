0919458e1fbd2035b9c78ec4e463a1595ceaf15d
Megt <megyltv@hotmail.com>
2017-02-07 14:10:57 -0500

    pagina inicio con rutas

---
+++ Mascotas/api/controllers/RutasController.js
@@ -0,0 +1,36 @@
+/**
+ * RutasController
+ *
+ * @description :: Server-side logic for managing Rutas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+    
+    home:function(req,res){
+            
+        // res.view(String: Nombre vista, Datos JSON)
+        
+        
+        
+        
+        
+        return res.view('vistas/home',{
+        })
+        
+    },
+    crearUsuario:function(req,res){
+        
+        return res.view('vistas/crearUsuario')
+        
+    }
+    
+};
+
+
+    
+    
+    
+    
+    
+    
