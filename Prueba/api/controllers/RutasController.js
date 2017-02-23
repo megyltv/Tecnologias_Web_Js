0f1379c3313520fdcfd167398c26d0a0ba5bacb3
Megt <megyltv@hotmail.com>
2017-02-23 12:59:13 -0500

    Creacion de api de materia, grupo

---
+++ Prueba/api/controllers/RutasController.js
@@ -0,0 +1,11 @@
+/**
+ * RutasController
+ *
+ * @description :: Server-side logic for managing Rutas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
