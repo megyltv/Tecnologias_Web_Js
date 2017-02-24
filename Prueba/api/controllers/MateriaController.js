c3ec687b238626bbfb40f4890f82531ddecbfab6
Megt <megyltv@hotmail.com>
2017-02-24 09:24:32 -0500

    create, delete, read

--- Prueba/api/controllers/MateriaController.js
+++ Prueba/api/controllers/MateriaController.js
@@ -6,6 +6,201 @@
  */
 
 module.exports = {
-	
-};
+	crearMateria: function (req, res) {
+		if (req.method == "POST") {
 
+			var parametros = req.allParams();
+
+			if (parametros.nombreMateria && parametros.topicoMateria && parametros.fechaCreacion) {
+
+				var materiaCrear = {
+					nombreMateria: parametros.nombreMateria,
+					topicoMateria: parametros.topicoMateria,
+					fechaCreacion: parametros.fechaCreacion
+				}
+
+				Materia.create(materiaCrear).exec(function (err, materiaCreada) {
+
+					if (err) {
+						return res.view('vistas/Error', {
+							error: {
+								desripcion: "Falla al crear nueva materia",
+								rawError: err,
+								url: "/CrearMateria"
+							}
+
+						});
+					}
+
+					Materia.find()
+						.exec(function (errorIndefinido, materiasEncontradas) {
+
+							if (errorIndefinido) {
+								res.view('vistas/Error', {
+									error: {
+										desripcion: "Hubo un problema listando las materias",
+										rawError: errorIndefinido,
+										url: "/ListarMaterias"
+									}
+								});
+							}
+
+							res.view('vistas/Materia/listarMaterias', {
+								materias: materiasEncontradas
+							});
+						})
+
+				})
+
+
+			} else {
+
+				return res.view('vistas/Error', {
+					error: {
+						desripcion: "Llena todos los parametros solicitados para la materia",
+						rawError: "Fallo en envio de parametros",
+						url: "/CrearMateria"
+					}
+
+				});
+
+			}
+
+
+		} else {
+
+			return res.view('vistas/Error', {
+				error: {
+					desripcion: "Error en el uso del Metodo HTTP",
+					rawError: "HTTP Invalido",
+					url: "/CrearMateria"
+				}
+			});
+
+		}
+	},
+	
+	BorrarMateria: function (req, res) {
+
+		var parametros = req.allParams();
+
+		if (parametros.id) {
+
+			Materia.destroy({
+				id: parametros.id
+			}).exec(function (errorInesperado, MateriaRemovida) {
+				if (errorInesperado) {
+					return res.view('vistas/Error', {
+						error: {
+							desripcion: "Tuvimos un Error Inesperado",
+							rawError: errorInesperado,
+							url: "/ListarMaterias"
+						}
+					});
+				}
+				Materia.find()
+					.exec(function (errorIndefinido, materiasEncontradas) {
+
+						if (errorIndefinido) {
+							res.view('vistas/Error', {
+								error: {
+									desripcion: "Hubo un problema listando las materias",
+									rawError: errorIndefinido,
+									url: "/ListarMaterias"
+								}
+							});
+						}
+
+						res.view('vistas/Materia/ListarMaterias', {
+							materias: materiasEncontradas
+						});
+					})
+			})
+
+		} else {
+			return res.view('vistas/Error', {
+				error: {
+					desripcion: "Necesitamos el ID para borrar la Materia",
+					rawError: "No envia ID",
+					url: "/ListarMaterias"
+				}
+			});
+		}
+	},
+	
+	editarMateria: function (req, res) {
+
+        var parametros = req.allParams();
+
+        if (parametros.idMateria && (parametros.nombreMateria || parametros.topicoMateria || parametros.fechaCreacion)) {
+            
+            var materiaAEditar = {
+                nombreMateria: parametros.nombreMateria,
+                topicoMateria: parametros.topicoMateria,
+                fechaCreacion: parametros.fechaCreacion
+            }
+
+            if (materiaAEditar.nombreMateria == "") {
+                delete materiaAEditar.nombreMateria
+            }
+            if (materiaAEditar.topicoMateria == "") {
+                delete materiaAEditar.topicoMateria
+            }
+            if (materiaAEditar.fechaCreacion == "") {
+                delete materiaAEditar.fechaCreacion
+            }
+            
+
+            Materia.update({
+                    id: parametros.idMateria
+                }, materiaAEditar)
+                .exec(function (errorInesperado, MateriaRemovida) {
+                    if (errorInesperado) {
+                        return res.view('vistas/Error', {
+                            error: {
+                                desripcion: "Tuvimos un Error Inesperado",
+                                rawError: errorInesperado,
+                                url: "/ListarMaterias"
+                            }
+                        });
+                    }
+                
+                    Materia.find()
+                        .exec(function (errorIndefinido, materiasEncontradas) {
+
+                            if (errorIndefinido) {
+                                res.view('vistas/Error', {
+                                    error: {
+                                        desripcion: "Hubo un problema listando las Materias",
+                                        rawError: errorIndefinido,
+                                        url: "/ListarMaterias"
+                                    }
+                                });
+                            }
+
+                            res.view('vistas/Materia/ListarMaterias', {
+                                materias: materiasEncontradas
+                            });
+                        })
+
+                })
+            
+            
+            
+            
+            
+
+        } else {
+            return res.view('vistas/Error', {
+                error: {
+                    desripcion: "Necesitamos que envies el ID y el nombre, topico o fecha de creacion de la materia",
+                    rawError: "No envia Parametros",
+                    url: "/ListarMaterias"
+                }
+            });
+        }
+
+
+
+    }
+};
--- Prueba/api/controllers/RutasController.js
+++ Prueba/api/controllers/RutasController.js
@@ -6,6 +6,108 @@
  */
 
 module.exports = {
+	home: function (req, res) {
+		// res.view(String: Nombre vista, Datos JSON)
+		return res.view('vistas/home', {})
+
+	},
+	crearMateria: function (req, res) {
+
+		return res.view('vistas/Materia/crearMateria')
+
+	},
+	
+	listarMaterias:function(req,res){
+		Materia.find()
+			.exec(function (errorIndefinido, materiasEncontradas) {
+
+				if (errorIndefinido) {
+					return res.view('vistas/Error', {
+						error: {
+							descripcion: "Hubo un problema listando las materias",
+							rawError: errorIndefinido,
+							url: "/ListarMaterias"
+						}
+					});
+				}
+
+				res.view('vistas/Materia/listarMaterias', {
+					materias: materiasEncontradas
+				})
+
+			})
+	},
+	
+	editarMateria: function(req,res){
+	
+		var parametros = req.allParams();
+
+		if (parametros.id) {
+
+			Materia.findOne({
+				id: parametros.id
+			}).exec(function (errorInesperado, MateriaEncontrada) {
+				if (errorInesperado) {
+					return res.view('vistas/Error', {
+						error: {
+							desripcion: "Error Inesperado",
+							rawError: errorInesperado,
+							url: "/ListarMaterias"
+						}
+					});
+				}
+				if (MateriaEncontrada) {
+					return res.view("vistas/Materia/editarMateria", {
+						materiaAEditar: MateriaEncontrada,
+						
+					});
+				} else {
+					return res.view('vistas/Error', {
+						error: {
+							desripcion: "La materia con id: " + parametros.id + " no existe.",
+							rawError: "No existe la materia",
+							url: "/ListarMaterias"
+						}
+					});
+				}
+			})
+		} else {
+
+			return res.view('vistas/Error', {
+				error: {
+					desripcion: "No ha enviado el parametro ID",
+					rawError: "Faltan Parametros",
+					url: "/ListarMaterias"
+				}
+			});
+
+		}
+	},
+	
+	
+	crearGrupo: function (req, res) {
+
+		return res.view('vistas/Grupo/crearGrupo')
+
+	},
+	
+	listarGrupos:function(req,res){
+		
+	},
+	
+	editarGrupo: function (req,res){
+		
+	},
+	
+	error: function(req,res){
+		return res.view('vistas/Error', {
+			error: {
+				descripcion: "Usted esta por error en esta Ruta, dirijase a Inicio",
+				rawError: "Ruta equivocada",
+				url: "/Inicio"
+			}
+		})
+	}
 	
 };
 
