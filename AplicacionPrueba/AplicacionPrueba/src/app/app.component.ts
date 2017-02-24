a7ca441de7b31450aa1c89348ea355b2127b4b7f
Megt <megyltv@hotmail.com>
2017-02-24 12:47:37 -0500

    Listar tiendas

--- AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
@@ -8,68 +8,49 @@ import {NgForm} from "@angular/forms";
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[MasterURLService]
+  providers: [MasterURLService]
 })
 // CTRL A +  -  CTRL + ALT + L
 export class AppComponent implements OnInit {
-  title: string = "Hola Amigos";
-  nombre: string = "";
-  apellido: string = "";
-  colorH4 = "red";
-  tamanoH4 = "52px";
-  classes = "btn btn-block btn-success";
-
-  error: string = "No hay errores";
-
-  nuevaTienda:any={};
-
-  disabledButtons={
-    NuevaTiendaFormSubmitButton:false
+  title: string = "Bienvenido a Ingresar Tiendas";
+  nuevaTienda = {};
+  tiendas = [];
+  disabledButtons = {
+    NuevaTiendaFormSubmitButton: false
   }
 
+  constructor(private _http: Http, private _masterURL: MasterURLService) {
 
-
-  constructor(private _http: Http, private _masterURL:MasterURLService) {
-    this.apellido = "Eguez";
-    this.nombre = "Adrian";
-    console.log("Inicio el construcor")
   }
 
   ngOnInit() {
-    this.apellido = "Sarzosa";
-    this.nombre = "Vicente";
-    console.log("On Init")
+    this._http.get(this._masterURL.url + "Tienda").subscribe(
+      (res: Response) => {
+        console.log(res.json());
+        this.tiendas = res.json();
+      },
+      (err) => {
+        console.log(err);
+      }
+    )
   }
 
-  nombreCompleto(): string {
-    return `${this.nombre} ${this.apellido}`
-  }
-
-  hizoClick() {
-    console.log("Hizo Click");
-    console.log()
-  }
-
-  hizoFocus() {
-    console.log("Hizo focus");
-  }
-
-  crearTienda(formulario:NgForm){
-    this.disabledButtons.NuevaTiendaFormSubmitButton=true;
+  crearTienda(formulario: NgForm) {
+    this.disabledButtons.NuevaTiendaFormSubmitButton = true;
     console.log(formulario);
-    this._http.post(this._masterURL.url+"Tienda",{
-      nombre:formulario.value.nombre
+    this._http.post(this._masterURL.url + "Tienda", {
+      nombre: formulario.value.nombre
     }).subscribe(
-      (res)=>{
-        console.log("No hubo Errores",res);
-        this.nuevaTienda={};
-        this.disabledButtons.NuevaTiendaFormSubmitButton=false;
+      (res) => {
+        console.log("No hubo Errores", res);
+        this.nuevaTienda = {};
+        this.disabledButtons.NuevaTiendaFormSubmitButton = false;
       },
-      (err)=>{
-        this.disabledButtons.NuevaTiendaFormSubmitButton=false;
-        console.log("Ocurrio un error",err);
+      (err) => {
+        this.disabledButtons.NuevaTiendaFormSubmitButton = false;
+        console.log("Ocurrio un error", err);
       },
-      ()=>{
+      () => {
         console.log("Termino la funcion");
       }
     );
@@ -84,4 +65,5 @@ export class AppComponent implements OnInit {
     //     }
     //   );
   }
+
 }
