966de1da6b5436da70d9229d4094901dbd746c30
Megt <megyltv@hotmail.com>
2017-02-24 11:13:26 -0500

    Crear tienda, habilitar botones

--- AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnInit} from "@angular/core";
 import {Response, Http} from "@angular/http";
+import {MasterURLService} from "./services/master-url.service";
 
 @Component({
+
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[MasterURLService]
 })
 // CTRL A +  -  CTRL + ALT + L
 export class AppComponent implements OnInit {
@@ -15,11 +18,13 @@ export class AppComponent implements OnInit {
   tamanoH4 = "52px";
   classes = "btn btn-block btn-success";
 
+  error: string = "No hay errores";
+
   nuevaTienda:any={};
 
 
 
-  constructor(private http: Http) {
+  constructor(private _http: Http, private _masterURL:MasterURLService) {
     this.apellido = "Eguez";
     this.nombre = "Adrian";
     console.log("Inicio el construcor")
@@ -44,17 +49,31 @@ export class AppComponent implements OnInit {
     console.log("Hizo focus");
   }
 
-
   crearTienda(formulario){
     console.log(formulario);
-    this.http
-      .post("http://localhost:1337/Tienda", formulario.valores)
-      .subscribe(
-        res=>console.log('Respuesta: ',res),
-        err=>console.log('Error: ',err),
-        ()=>{
-          console.log("Se completo la accion")
-        }
-      );
+    this._http.post(this._masterURL.url+"Tienda",{
+      nombre:formulario.value.nombre
+    }).subscribe(
+      (res)=>{
+        console.log("No hubo Errores",res);
+        this.nuevaTienda={};
+      },
+      (err)=>{
+        console.log("Ocurrio un error",err);
+      },
+      ()=>{
+        console.log("Termino la funcion");
+      }
+    );
+
+    // this.http
+    //   .post("http://localhost:1337/Tienda", formulario.valores)
+    //   .subscribe(
+    //     res=>console.log('Respuesta: ',res),
+    //     err=>console.log('Error: ',err),
+    //     ()=>{
+    //       console.log("Se completo la accion")
+    //     }
+    //   );
   }
 }
